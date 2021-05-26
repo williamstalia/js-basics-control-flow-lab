8b00f9192a8c01a7a3069cd93d983de9904377f7
Talia Williams <williams.talia101@gmail.com>
2021-05-26 03:15:35 -0500

    commit tree 6d7e5bd81e14333f454106be6cb44126be180aa2

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){ 
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks." ;
+  } else {
+    return "No can do.";
+  }
+  
+  
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+
+
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go."
+  }
+  }
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  function switchOnCharmFromTip(tip) {
+    switch (tip){
+      case "generous":
+        return "Thank you so much."
+        break;
+        
+      case "not as generous":
+          return "Thank you."
+          break;
+      default:
+        return "Bye."
+        break;
+    }
+  }
 }
